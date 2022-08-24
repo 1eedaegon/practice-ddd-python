d1e7b96e4c778f104799305c58af46daa9159426
1eedaegon <d8726243@gmail.com>
2022-08-24 22:05:18 +0900

    Fix repository and query error

--- tests/test_repository.py
+++ tests/test_repository.py
@@ -6,7 +6,7 @@ def test_repository_can_save_a_batch(session):
     batch = model.Batch("batch-1", "RUSTY-SOAPDISH", 100, eta=None)
     repo = repository.SqlAlchemyRepository(session)
     repo.add(batch)
-    repo.commit()
+    session.commit()
 
     rows = list(session.execute('SELECT reference, sku, _purchased_quantity, eta FROM "batches"'))
     assert rows == [("batch-1", "RUSTY-SOAPDISH", 100, None)]
@@ -14,19 +14,19 @@ def test_repository_can_save_a_batch(session):
 
 def insert_order_line(session):
     session.execute("INSERT INTO order_lines (orderid, sku, qty)" 'VALUES ("order1", "GENERIC-SOFA", 12)')
-    [[orderline_id]] = session.execute("SELECT id FROM order_lines WHERE orderid:=orderid AND sku:=sku,", dict(orderid="order1", sku="GENERIC-SOFA"))
+    [[orderline_id]] = session.execute("SELECT id FROM order_lines WHERE orderid=:orderid AND sku=:sku", dict(orderid="order1", sku="GENERIC-SOFA"))
     return orderline_id
 
 
 def insert_batch(session, batch_id):
     session.execute(
-        "INSERT INTO batches (reference, sku, _purchased_quantity, eta) FROM batches" 'VALUES (:batch_id, "GENERIC-SOFA", 100, null)',
+        "INSERT INTO batches (reference, sku, _purchased_quantity, eta)" 'VALUES (:batch_id, "GENERIC-SOFA", 100, null)',
         dict(
             batch_id=batch_id,
         ),
     )
 
-    [[batch_id]] = session.execute('SELECT id FROM batches WHERE reference:=batch_id AND sku="GENERIC-SOFA"', dict(batch_id=batch_id))
+    [[batch_id]] = session.execute('SELECT id FROM batches WHERE reference=:batch_id AND sku="GENERIC-SOFA"', dict(batch_id=batch_id))
     return batch_id
 
 
@@ -49,4 +49,4 @@ def test_repository_can_retrieve_a_batch_with_allocations(session):
     assert retrieved == expected
     assert retrieved.sku == expected.sku
     assert retrieved._purchased_quantity == expected._purchased_quantity
-    assert retrieved._allocations == {model.OrderLine("order1", "GENERIC-SOFA", 100, 12)}
+    assert retrieved._allocations == {model.OrderLine("order1", "GENERIC-SOFA", 12)}
